23523b242153b9bb2191339752f157ae47daaa35
Jay Jaiswal <jaiswaljay010@gmail.com>
2026-04-04 08:40:23 +0530

    fix CORS for vercel deploy

--- backend/main.py
+++ backend/main.py
@@ -90,6 +90,8 @@ app.add_middleware(
         "http://localhost:3000",   # Next.js dev
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
+        "https://crafta-asz495d55-jay-jaiswals-projects.vercel.app",
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
